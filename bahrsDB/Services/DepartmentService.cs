95dc15b2ed0f9b9a299e86d26edee301c3e175d4
Marcelino Rodrigues <marcelino.rodrigues.d.n@gmail.com>
2021-02-07 21:28:46 -0300

    operaçoes assincronas

--- bahrsDB/Services/DepartmentService.cs
+++ bahrsDB/Services/DepartmentService.cs
@@ -1,7 +1,9 @@
 ﻿using bahrsDB.Data;
 using bahrsDB.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace bahrsDB.Services
 {
@@ -14,9 +16,9 @@ namespace bahrsDB.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
