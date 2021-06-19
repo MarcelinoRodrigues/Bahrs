be4a5bca6638f519a648be074543509407988e8c
Marcelino Rodrigues <marcelino.rodrigues.d.n@gmail.com>
2021-06-19 11:19:05 -0300

    adicionando todas as tabelas atuais

--- bahrsDB/copia.sql
+++ bahrsDB/copia.sql
@@ -1,4 +1,4 @@
-﻿USE [SystemMRN]
+﻿
 GO
 
 /****** Object:  Table [dbo].[Vehicle]    Script Date: 25/05/2021 20:59:01 ******/
@@ -24,7 +24,6 @@ ALTER TABLE [dbo].[Vehicle] ADD  DEFAULT ((0)) FOR [Status]
 GO
 
 
-USE [SystemMRN]
 GO
 
 /****** Object:  Table [dbo].[Vacancy]    Script Date: 25/05/2021 20:59:33 ******/
@@ -47,5 +46,94 @@ GO
 
 ALTER TABLE [dbo].[Vacancy] ADD  DEFAULT ((0)) FOR [Status]
 GO
+
+GO
+
+/****** Object:  Table [dbo].[Employee]    Script Date: 19/06/2021 11:10:35 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Employee](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Nome] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+GO
+
+/****** Object:  Table [dbo].[User]    Script Date: 19/06/2021 11:12:11 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[User](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Nome] [varchar](50) NOT NULL,
+	[Senha] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+GO
+
+/****** Object:  Table [dbo].[Parking]    Script Date: 19/06/2021 11:12:44 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Parking](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Entrada] [datetime] NOT NULL,
+	[Vencimento] [datetime] NULL,
+	[Valor] [decimal](18, 0) NOT NULL,
+	[VeiculoId] [int] NOT NULL,
+	[FuncionarioId] [int] NOT NULL,
+	[VagaId] [int] NOT NULL,
+ CONSTRAINT [PK_Parking] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+ CONSTRAINT [AK_vaga] UNIQUE NONCLUSTERED 
+(
+	[VagaId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+ CONSTRAINT [AK_veiculo] UNIQUE NONCLUSTERED 
+(
+	[VeiculoId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Parking]  WITH CHECK ADD  CONSTRAINT [FK_parking] FOREIGN KEY([VagaId])
+REFERENCES [dbo].[Vacancy] ([Id])
+GO
+
+ALTER TABLE [dbo].[Parking] CHECK CONSTRAINT [FK_parking]
+GO
+
+ALTER TABLE [dbo].[Parking]  WITH CHECK ADD  CONSTRAINT [FK_parking_veiculo] FOREIGN KEY([VeiculoId])
+REFERENCES [dbo].[Vehicle] ([Id])
+GO
+
+ALTER TABLE [dbo].[Parking] CHECK CONSTRAINT [FK_parking_veiculo]
+GO
+
+
 
 
