d4ff5a677e622f099ca2889acaa7702478b3294d
Marcelino Rodrigues <marcelino.rodrigues.d.n@gmail.com>
2021-06-20 10:31:55 -0300

    melhorando a performace do UserController

--- bahrsDB/Controllers/UsersController.cs
+++ bahrsDB/Controllers/UsersController.cs
@@ -7,45 +7,15 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using bahrsDB.Data;
 using bahrsDB.Models;
+using bahrsDB.Data.Base;
 
 namespace bahrsDB.Controllers
 {
-    public class UsersController : Controller
+    public class UsersController : BaseController<User>
     {
-        private readonly bahrsDBContext _context;
 
-        public UsersController(bahrsDBContext context)
+        public UsersController(bahrsDBContext context) : base(context)
         {
-            _context = context;
-        }
-
-        // GET: Users
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.User.ToListAsync());
-        }
-
-
-        // GET: Users/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Users/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Nome,Senha")] User user)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(user);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(user);
         }
 
         private bool UserExists(int id)
