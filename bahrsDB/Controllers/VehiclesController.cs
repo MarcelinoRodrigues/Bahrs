7bfec719e0accab35b8edede998a946139b40c43
Diego Gaspar <diego_gaspar25@hotmail.com>
2021-06-19 21:44:32 -0300

    Criação da base controller ajuste no redirecionamento e inclusão do alertjs

--- bahrsDB/Controllers/VehiclesController.cs
+++ bahrsDB/Controllers/VehiclesController.cs
@@ -1,119 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using bahrsDB.Data;
+using bahrsDB.Data.Base;
 using bahrsDB.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace bahrsDB.Controllers
 {
-    public class VehiclesController : Controller
+    public class VehiclesController : BaseController<Vehicle>
     {
-        private readonly bahrsDBContext _context;
-
-        public VehiclesController(bahrsDBContext context)
-        {
-            _context = context;
+        public VehiclesController(bahrsDBContext context): base(context)
+        {           
         }
-
-        // GET: Vehicles
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Vehicle.ToListAsync());
-        }
-
-        // GET: Vehicles/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var vehicle = await _context.Vehicle
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (vehicle == null)
-            {
-                return NotFound();
-            }
-
-            return View(vehicle);
-        }
-
-        // GET: Vehicles/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Vehicles/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Nome,Placa")] Vehicle vehicle)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(vehicle);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(vehicle);
-        }
-
-        // GET: Vehicles/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var vehicle = await _context.Vehicle.FindAsync(id);
-            if (vehicle == null)
-            {
-                return NotFound();
-            }
-            return View(vehicle);
-        }
-
-        // POST: Vehicles/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Placa")] Vehicle vehicle)
-        {
-            if (id != vehicle.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(vehicle);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!VehicleExists(vehicle.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(vehicle);
-        }
-
+       
         // GET: Vehicles/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        public override async Task<IActionResult> Delete(string id)
         {
             if (id == null)
             {
@@ -121,7 +23,7 @@ namespace bahrsDB.Controllers
             }
 
             var vehicle = await _context.Vehicle
-                .FirstOrDefaultAsync(m => m.Id == id);
+                .FirstOrDefaultAsync(m => m.Id == int.Parse(id));
             if (vehicle == null)
             {
                 return NotFound();
@@ -143,6 +45,8 @@ namespace bahrsDB.Controllers
             //se estiver associada retorna a pagina
             if (verificaSeEstarAssociado)
             {
+
+                TempData["Mensagem"] = "O veículo ainda está vinculado a uma vaga.";
                 return RedirectToAction(nameof(Index));
             }
 
---
+++ bahrsDB/Data/Base/BaseController.cs
@@ -0,0 +1,162 @@
+﻿/******
+ * Classe base para os controllers
+ * Diego Gaspar
+ * ****/
+
+
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace bahrsDB.Data.Base
+{
+
+    public abstract class BaseController<T> : Controller where T : class
+    {
+        protected readonly bahrsDBContext _context;
+
+        protected BaseController(bahrsDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public virtual async Task<IActionResult> Index()
+        {
+            var lista = await _context.Set<T>().ToListAsync();
+
+            return View(lista);
+        }
+
+        [HttpGet]
+        public virtual async Task<IActionResult> Details(string id)
+        {
+            T objeto = await GetObjeto(id);
+
+            if (objeto != null)
+                return View(objeto);
+
+            TempData["Mensagem"] = "Registro não encontrado";
+
+            return RedirectToAction("index");
+        }
+
+        [HttpGet]
+        public virtual IActionResult Create()
+        {
+            return View();
+        }
+
+        public virtual async Task<IActionResult> Create(T objeto)
+        {
+            if (objeto == null)
+                TempData["Mensagem"] = "Registro não encontrado";
+            else
+            {
+                SetProp(objeto, "Ativo", "DoubleCheck");
+
+                await _context.AddAsync(objeto);
+                await _context.SaveChangesAsync();
+
+                TempData["Mensagem"] = "Operação realizada com sucesso.";
+            }
+
+            return RedirectToAction("index");
+        }
+
+        [HttpGet]
+        public virtual async Task<IActionResult> Edit(string id)
+        {
+            T objeto = await GetObjeto(id);
+
+            if (objeto != null)
+                return View(objeto);
+
+
+            TempData["Mensagem"] = "Registro não encontrado";
+
+
+            return RedirectToAction("index");
+        }
+
+        [HttpPost]
+        public virtual IActionResult Edit(T objeto)
+        {
+            if (objeto == null)
+                TempData["Mensagem"] = "Registro não encontrado";
+            else
+            {
+                SetProp(objeto, "Ativo", "DoubleCheck");
+
+                _context.Update(objeto);
+                _context.SaveChanges();
+
+                TempData["Mensagem"] = "Operação realizada com sucesso.";
+            }
+
+            return RedirectToAction("index");
+        }
+
+        [HttpGet]
+        public virtual async Task<IActionResult> Delete(string id)
+        {
+
+            T objeto =await GetObjeto(id);
+
+            if (objeto == null)
+                TempData["Mensagem"] = "Registro não encontrado";
+            else
+            {
+                _context.Remove(objeto);
+                _context.SaveChanges();
+                TempData["Mensagem"] = "Operação realizada com sucesso.";
+            }
+            return RedirectToAction("index");
+        }
+
+        public virtual async Task<T> GetObjeto(string id)
+        {
+            int IdAux = 0;
+            int.TryParse(id, out IdAux);
+
+
+            T objeto = null;
+
+
+            //Busca nas tabelas que tem ID tipo inteiro
+            if (IdAux > 0)
+                objeto = await _context.Set<T>().FindAsync(IdAux);
+
+            //Busca nas tabelas que tem ID tipo string
+            else if (!string.IsNullOrWhiteSpace(id) && id != "0")
+                objeto = await _context.Set<T>().FindAsync(id);
+
+
+            return objeto;
+        }
+
+        protected void SetProp(T objeto, params string[] props)
+        {
+            foreach (var prop in props)
+            {
+                var propName = objeto
+                                    .GetType()
+                                    .GetProperty(prop)?.Name;
+
+                if (!string.IsNullOrWhiteSpace(propName))
+                {
+                    var isChecked = objeto
+                                    .GetType()
+                                    .GetProperty(propName)
+                                    .GetValue(objeto);
+
+                    objeto
+                        .GetType()
+                        .GetProperty(propName)
+                        .SetValue(objeto, isChecked ?? false);
+                }
+            }
+        }
+    }
+}
